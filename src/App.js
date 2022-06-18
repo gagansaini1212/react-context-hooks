40ac4f06ee8161b97c6b1b56a5bc902d0c61eb56
gagansaini1212 <gagansaini1212@gmail.com>
2022-06-18 21:56:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import ThemeContextProvider from './contexts/ThemeContext';
+import Navbar from './components/Navbar';
+import BookList from './components/BookList';
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
+    </div>
+  );
+}
+
+export default App;
