265ff746cf74e4edcd0a8c292c02cf604d15c6e8
gagansaini1212 <gagansaini1212@gmail.com>
2022-06-19 11:30:31 +0530

    creating multiple contexts

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import React from 'react';
 import ThemeContextProvider from './contexts/ThemeContext';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
-import ThemeToggle from './contexts/ThemeToggle';
+import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
 
 function App() {
+  console.log(AuthContextProvider);
   return (
     <div className="App">
       <ThemeContextProvider>
+        {/* <AuthContextProvider> */}
         <Navbar />
         <BookList />
         <ThemeToggle />
+        {/* </AuthContextProvider> */}
       </ThemeContextProvider>
     </div>
   );
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,24 @@
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext();
+
+class AuthContextProvider extends React.Component {
+  state = {
+    isAuthenticated: false,
+  };
+
+  toggleAuth = () => {
+    this.setState({ isAuthenticated: !this.state.isAuthenticated });
+  };
+  render() {
+    return (
+      <AuthContextProvider
+        value={{ ...this.state, toggleAuth: this.toggleAuth }}
+      >
+        {this.props.children}
+      </AuthContextProvider>
+    );
+  }
+}
+
+export default AuthContextProvider;
